b1650313a886191606e0b17eff6c534ad8e42dd8
Alex Rincon <alejandro.rincon9@gmail.com>
2017-06-23 21:56:48 -0500

    Edit stores and add address

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,17 @@ exports.getStores = async (req, res) => {
   const stores =  await Store.find();
   res.render('stores', { title: 'Stores',  stores });
 };
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new object instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`)
+  res.redirect(`/stores/${store.id}/edit`)
+};
